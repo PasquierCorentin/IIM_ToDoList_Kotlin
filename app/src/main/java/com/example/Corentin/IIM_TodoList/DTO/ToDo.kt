8e66babc4271491ab90b5fc303ee2cf5439ad605
PasquierCorentin <43130282+CorentinPasquier78@users.noreply.github.com>
2020-01-05 17:55:36 +0100

    Initial commit

---
+++ app/src/main/java/com/example/Corentin/IIM_TodoList/DTO/ToDo.kt
@@ -0,0 +1,10 @@
+package com.example.Corentin.IIM_TodoList.DTO
+
+class ToDo {
+
+    var id: Long = -1
+    var name = ""
+    var createdAt = ""
+    var items: MutableList<ToDoItem> = ArrayList()
+
+}
---
+++ app/src/main/java/com/example/Corentin/IIM_TodoList/DTO/ToDoItem.kt
@@ -0,0 +1,10 @@
+package com.example.Corentin.IIM_TodoList.DTO
+
+class ToDoItem(){
+
+    var id : Long = -1
+    var toDoId : Long = -1
+    var itemName = ""
+    var isCompleted = false
+
+}
